cba22facafa2b6c274fbf73b738b3ad597d56fca
Robert Anderson <robert.anderson@microsoft.com>
2016-06-06 11:00:00 -0700

    Modify new base request classes for function overloads

--- src/Microsoft.Graph.Core/Requests/BaseGetMethodRequestBuilder.cs
+++ src/Microsoft.Graph.Core/Requests/BaseGetMethodRequestBuilder.cs
@@ -4,31 +4,26 @@
 
 namespace Microsoft.Graph
 {
-    using System;
     using System.Collections.Generic;
 
     /// <summary>
     /// The base method request builder class.
     /// </summary>
-    public abstract class BaseMethodRequestBuilder<T> : BaseRequestBuilder
-        where T : IBaseRequest
+    public abstract class BaseGetMethodRequestBuilder<T> : BaseRequestBuilder where T : IBaseRequest
     {
+        private List<string> _parameters = new List<string>();
+
         /// <summary>
         /// Constructs a new <see cref="BaseMethodRequestBuilder"/>.
         /// </summary>
         /// <param name="requestUrl">The URL for the request.</param>
         /// <param name="client">The <see cref="IBaseClient"/> for handling requests.</param>
-        public BaseMethodRequestBuilder(
+        public BaseGetMethodRequestBuilder(
             string requestUrl,
             IBaseClient client)
             : base(requestUrl, client)
         {
         }
-
-        /// <summary>
-        /// Gets or sets the designated request function for this method request builder.
-        /// </summary>
-        protected Func<IEnumerable<Option>, T> RequestFunction { get; set; }
 
         /// <summary>
         /// Derived classes implement this function to construct the specific request class instance
@@ -46,29 +41,17 @@ namespace Microsoft.Graph
         /// <returns>The built request.</returns>
         public T Request(IEnumerable<Option> options = null)
         {
-            return RequestFunction(options);
-        }
-
-        /// <summary>
-        /// Boiler plate helper code that is common to all request construction operations.
-        /// This method constructs a request URL and constructs the request class instance.
-        /// </summary>
-        /// <param name="parameters">The set of parameters to pass to invoked OData method.</param>
-        /// <param name="options">The query and header options for the request.</param>
-        /// <returns></returns>
-        protected T RequestCore(string[] parameters, IEnumerable<Option> options)
-        {
             string fnUrl = this.RequestUrl;
-            if (parameters != null)
+            if (_parameters.Count > 0)
             {
-                fnUrl = string.Format("{0}({1})", fnUrl, string.Join(",", parameters));
+                fnUrl = string.Format("{0}({1})", fnUrl, string.Join(",", _parameters));
             }
 
             return CreateRequest(fnUrl, options);
         }
 
         /// <summary>
-        /// A helper method for constructing a parameter string for a given name and value
+        /// A helper method for injecting a parameter string for a given name and value
         /// pair. This method handles the nullable case and properly wrapped and escaping
         /// string values.
         /// </summary>
@@ -76,25 +59,25 @@ namespace Microsoft.Graph
         /// <param name="value">The parameter value.</param>
         /// <param name="nullable">A flag specifying whether the parameter is allowed to be null.</param>
         /// <returns>A string representing the parameter for an OData method call.</returns>
-        protected string GetWrappedParameter(string name, object value, bool nullable)
+        protected void SetParameter(string name, object value, bool nullable)
         {
-            if (value != null || nullable)
+            if (value == null && !nullable)
             {
-                string valueAsString = value != null ? value.ToString() : "null";
-                if (value != null && value is string)
-                {
-                    valueAsString = "'" + EscapeStringValue(valueAsString) + "'";
-                }
-
-                return string.Format("{0}={1}", name, valueAsString);
+                throw new ServiceException(
+                    new Error
+                    {
+                        Code = "invalidRequest",
+                        Message = string.Format("{0} is a required parameter for this method request.", name),
+                    });
             }
 
-            throw new ServiceException(
-                new Error
-                {
-                    Code = "invalidRequest",
-                    Message = string.Format("{0} is a required parameter for this method request.", name),
-                });
+            string valueAsString = value != null ? value.ToString() : "null";
+            if (value != null && value is string)
+            {
+                valueAsString = "'" + EscapeStringValue(valueAsString) + "'";
+            }
+
+            _parameters.Add(string.Format("{0}={1}", name, valueAsString));
         }
 
         /// <summary>
