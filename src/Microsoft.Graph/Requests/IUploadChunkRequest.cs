7c037cf15413b73d1ad7b52ee027a703f4b63be1
Michael Mainer <eyemichael@hotmail.com>
2016-10-05 15:07:17 -0700

    Added manual requests and helpers for uploading file chunks. Added test to upload file.

---
+++ src/Microsoft.Graph/Requests/IUploadChunkRequest.cs
@@ -0,0 +1,21 @@
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+namespace Microsoft.Graph
+{
+    using System.IO;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The interface IUploadChunkRequest.
+    /// </summary>
+    public partial interface IUploadChunkRequest : IBaseRequest
+    {
+        /// <summary>
+        /// Puts the specified Chunk.
+        /// </summary>
+        /// <returns>The task to await.</returns>
+        Task<UploadChunkResult> PutAsync(Stream stream);
+    }
+}
---
+++ tests/Microsoft.Graph.Test/Requests/Functional/OneDriveTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Microsoft.Graph.Test.Requests.Functional
+{
+    //[Ignore]
+    [TestClass]
+    public class OneDriveTests : GraphTestBase
+    {
+        // https://github.com/OneDrive/onedrive-sdk-csharp/blob/master/docs/chunked-uploads.md
+        // https://dev.onedrive.com/items/upload_large_files.htm
+        [TestMethod]
+        public async Task OneDriveUploadLargeFile()
+        {
+            try
+            {
+                
+
+                System.Drawing.ImageConverter converter = new System.Drawing.ImageConverter();
+                var buff = (byte[])converter.ConvertTo(Microsoft.Graph.Test.Properties.Resources.hamilton, typeof(byte[]));
+                using (System.IO.MemoryStream ms = new System.IO.MemoryStream(buff))
+                {
+                    // Describe the file to upload. Pass into CreateUploadSession, when the service works as expected.
+                    //var props = new DriveItemUploadableProperties();
+                    //props.Name = "_hamilton.png";
+                    //props.Description = "This is a pictureof Mr. Hamilton.";
+                    //props.FileSystemInfo = new FileSystemInfo();
+                    //props.FileSystemInfo.CreatedDateTime = System.DateTimeOffset.Now;
+                    //props.FileSystemInfo.LastModifiedDateTime = System.DateTimeOffset.Now;
+
+
+                    // Get the provider. This one done with the id of root
+                    // POST /v1.0/drive/items/01KGPRHTV6Y2GOVW7725BZO354PWSELRRZ:/_hamiltion.png:/microsoft.graph.createUploadSession
+                    // The CreateUploadSesssion action doesn't seem to support the options s    tated in the metadata.
+                    var uploadSession = await graphClient.Drive.Items["01KGPRHTV6Y2GOVW7725BZO354PWSELRRZ"].ItemWithPath("_hamilton.png").CreateUploadSession().Request().PostAsync();
+
+                    var maxChunkSize = 320 * 1024; // 320 KB
+                    var provider = new ChunkedUploadProvider(uploadSession, graphClient, ms, maxChunkSize);
+
+                    // Setup the chunk request necessities
+                    var chunkRequests = provider.GetUploadChunkRequests();
+                    var readBuffer = new byte[maxChunkSize];
+                    var trackedExceptions = new List<Exception>();
+                    DriveItem itemResult = null;
+
+                    //upload the chunks
+                    foreach (var request in chunkRequests)
+                    {
+                        // Do your updates here: update progress bar, etc.
+                        // ...
+                        // Send chunk request
+                        var result = await provider.GetChunkRequestResponseAsync(request, readBuffer, trackedExceptions);
+
+                        if (result.UploadSucceeded)
+                        {
+                            itemResult = result.ItemResponse;
+                        }
+                    }
+
+                    // Check that upload succeeded
+                    if (itemResult == null)
+                    {
+                        // Retry the upload
+                        // ...
+                    }
+                }
+
+                
+            }
+            catch (Microsoft.Graph.ServiceException e)
+            {
+                Assert.Fail("Something happened, check out a trace. Error code: {0}", e.Error.Code);
+            }
+        }
+
+
+    }
+}
