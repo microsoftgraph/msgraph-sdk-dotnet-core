0d6c7d53432749ceb604afa4c4ff13229370a9ff
Michael Mainer <eyemichael@hotmail.com>
2016-10-17 15:58:08 -0700

    Bumped the minor version

--- src/Microsoft.Graph/Properties/AssemblyInfo.cs
+++ src/Microsoft.Graph/Properties/AssemblyInfo.cs
@@ -24,8 +24,8 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 #if DEBUG
 [assembly: InternalsVisibleTo("Microsoft.Graph.Test")]
