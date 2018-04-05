a5bc7c7329581cd794ca522e205a7832689445da
Caitlin Bales <c.bales@outlook.com>
2018-04-04 18:27:26 -0700

    Option comments

--- src/Microsoft.Graph.Core/Requests/Options/HeaderOption.cs
+++ src/Microsoft.Graph.Core/Requests/Options/HeaderOption.cs
@@ -4,8 +4,16 @@
 
 namespace Microsoft.Graph
 {
+    /// <summary>
+    /// A key value pair to be added to the request headers.
+    /// </summary>
     public class HeaderOption : Option
     {
+        /// <summary>
+        /// Create a header option.
+        /// </summary>
+        /// <param name="name">The name, or key, of the header option.</param>
+        /// <param name="value">The value for the header option.</param>
         public HeaderOption(string name, string value)
             : base(name, value)
         {
--- src/Microsoft.Graph.Core/Requests/Options/Option.cs
+++ src/Microsoft.Graph.Core/Requests/Options/Option.cs
@@ -4,16 +4,30 @@
 
 namespace Microsoft.Graph
 {
+    /// <summary>
+    /// A key value pair to be added to the request.
+    /// </summary>
     public abstract class Option
     {
+        /// <summary>
+        /// Create a new option.
+        /// </summary>
+        /// <param name="name">The name of the option.</param>
+        /// <param name="value">The value of the option.</param>
         protected Option(string name, string value)
         {
             this.Name = name;
             this.Value = value;
         }
 
+        /// <summary>
+        /// The name, or key, of the option.
+        /// </summary>
         public string Name { get; private set; }
 
+        /// <summary>
+        /// The value of the option.
+        /// </summary>
         public string Value { get; private set; }
     }
 }
