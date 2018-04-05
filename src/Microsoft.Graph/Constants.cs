f48e183c34b16b925749baff99bb61ad2cfd9097
Caitlin Bales <c.bales@outlook.com>
2018-04-04 17:05:21 -0700

    Code comments for constants

--- src/Microsoft.Graph/Constants.cs
+++ src/Microsoft.Graph/Constants.cs
@@ -4,20 +4,41 @@
 
 namespace Microsoft.Graph
 {
+    /// <summary>
+    /// Constants used for navigating Graph
+    /// </summary>
     public static class Constants
     {
+        /// <summary>
+        /// URL segment constants
+        /// </summary>
         public static class Url
         {
+            /// <summary>
+            /// The AppRoot folder
+            /// </summary>
             public const string AppRoot = "approot";
 
+            /// <summary>
+            /// The Deleted Items folder
+            /// </summary>
             public const string DeletedItems = "DeletedItems";
 
             public const string Drafts = "Drafts";
 
+            /// <summary>
+            /// The base URL format for Graph
+            /// </summary>
             public const string GraphBaseUrlFormatString = "https://graph.microsoft.com/{0}";
 
+            /// <summary>
+            /// The Inbox folder
+            /// </summary>
             public const string Inbox = "Inbox";
 
+            /// <summary>
+            /// The Sent Items folder
+            /// </summary>
             public const string SentItems = "SentItems";
         }
     }
