f807196101e20d30fbc8628206a2eb5850334a92
Michael Mainer <MIchaelMainer@users.noreply.github.com>
2018-06-28 14:37:27 -0700

    Updated System.Net.Http from 4.3.1 to 4.3.3 (#279)

--- src/Microsoft.Graph/Properties/AssemblyInfo.cs
+++ src/Microsoft.Graph/Properties/AssemblyInfo.cs
@@ -24,8 +24,8 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.9.0")]
-[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyVersion("1.9.1")]
+[assembly: AssemblyFileVersion("1.9.1.0")]
 
 #if DEBUG
 [assembly: InternalsVisibleTo("Microsoft.Graph.Test")]
