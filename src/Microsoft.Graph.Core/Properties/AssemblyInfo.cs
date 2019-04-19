5e2aa29b6cd1e84c01447c96e476a7aab5ab6738
Peter Ombwa <peter.ombwa@microsoft.com>
2019-04-18 17:08:51 -0700

    1.15.0 preview 1 release notes (#445)
    * Update release notes.

--- src/Microsoft.Graph.Core/Properties/AssemblyInfo.cs
+++ src/Microsoft.Graph.Core/Properties/AssemblyInfo.cs
@@ -1,32 +1,6 @@
 ﻿using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Microsoft Graph Core Client Library")]
-[assembly: AssemblyDescription("This package includes the core client libraries for connecting your app to the Microsoft Graph. Microsoft Graph enables your app to interact with data entities and their relationships from many Microsoft cloud services with a unified developer experience. Learn more about Microsoft Graph at http://graph.microsoft.io.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("Microsoft.Graph.Core")]
-[assembly: AssemblyCopyright("Copyright © Microsoft Corporation")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.14.0")]
-[assembly: AssemblyFileVersion("1.14.0.0")]
-
 #if DEBUG
 [assembly: InternalsVisibleTo("Microsoft.Graph.Core.Test")]
 [assembly: InternalsVisibleTo("Microsoft.Graph.Test")]
