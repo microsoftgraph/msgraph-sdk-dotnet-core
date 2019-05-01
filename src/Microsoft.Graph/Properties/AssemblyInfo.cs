da8cb814f66d08c4bbff3e54e4a305eb890aa8df
Michael Mainer <MIchaelMainer@users.noreply.github.com>
2019-04-30 17:31:22 -0700

    Final update for Build (#456)
    * Update. M.G.Core to 1.15.0
    * Update M.G to 1.15.0, update build to use csproj def for assembly
    * Updated release notes.

--- src/Microsoft.Graph/Properties/AssemblyInfo.cs
+++ src/Microsoft.Graph/Properties/AssemblyInfo.cs
@@ -1,31 +1,4 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Microsoft Graph Client Library")]
-[assembly: AssemblyDescription("This package includes the client libraries for connecting your app to the Microsoft Graph. Microsoft Graph enables your app to interact with data entities and their relationships from many Microsoft cloud services with a unified developer experience. Learn more about Microsoft Graph at http://graph.microsoft.io.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("Microsoft.Graph")]
-[assembly: AssemblyCopyright("Copyright © Microsoft Corporation")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.14.0")]
-[assembly: AssemblyFileVersion("1.14.0.0")]
+﻿using System.Runtime.CompilerServices;
 
 #if DEBUG
 [assembly: InternalsVisibleTo("Microsoft.Graph.Test")]
