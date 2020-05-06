159c814a1cfaa8621c56961ce924e19dee51064a
Vincent Biret <vincentbiret@hotmail.com>
2020-05-06 11:05:10 -0400

    - adds certificate serialization for subscription

---
+++ src/Microsoft.Graph.Core/Models/EncryptableSubscription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Microsoft.Graph
+{
+    public abstract class EncryptableSubscription : IEncryptableSubscription
+    {
+        public abstract string EncryptionCertificate { get; set; }
+
+        /// <summary>
+        /// Adds the encryption certificate information for change notifications with resource data to the subscription creation information.
+        /// </summary>
+        /// <param name="certificate">Certificate to use for encryption</param>
+        public void AddEncryptionCertificate(X509Certificate2 certificate)
+        {
+            EncryptionCertificate = Convert.ToBase64String(certificate.Export(X509ContentType.SerializedCert));
+        }
+    }
+}
---
+++ src/Microsoft.Graph.Core/Models/IEncryptableSubscription.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Microsoft.Graph
+{
+    public interface IEncryptableSubscription
+    {
+        string EncryptionCertificate { get; set; }
+
+        void AddEncryptionCertificate(X509Certificate2 certificate);
+    }
+}
