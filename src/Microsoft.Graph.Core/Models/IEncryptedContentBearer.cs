d7d5b53a9208a340f5efba86c749cc5faf7bbd1d
Vincent Biret <vincentbiret@hotmail.com>
2020-05-06 11:06:31 -0400

    - adds notification collection token validation

---
+++ src/Microsoft.Graph.Core/Models/IEncryptedContentBearer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Graph
+{
+    public interface IEncryptedContentBearer
+    {
+        IDecryptableContent EncryptedContent { get; set; }
+    }
+}
---
+++ src/Microsoft.Graph.Core/Models/ITokenValidable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.Graph
+{
+    public interface ITokenValidable
+    {
+        IEnumerable<string> ValidationTokens { get; set; }
+        IEnumerable<IEncryptedContentBearer> Value { get; set; }
+
+        Task<bool> AreTokensValid(IEnumerable<Guid> tenantIds, IEnumerable<Guid> appIds, string wellKnownUri = "https://login.microsoftonline.com/common/.well-known/openid-configuration", string issuerPrefix = "https://sts.windows.net/");
+    }
+}
