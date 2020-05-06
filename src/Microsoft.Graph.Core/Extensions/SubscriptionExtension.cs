96c15aa07c15aeedc5fd0ab990b05cc63bf86e58
Vincent Biret <vincentbiret@hotmail.com>
2020-05-06 11:11:46 -0400

    - moves certificate encryption method to extension to solve double inheritance

--- src/Microsoft.Graph.Core/Extensions/SubscriptionExtension.cs
+++ src/Microsoft.Graph.Core/Extensions/SubscriptionExtension.cs
@@ -3,17 +3,15 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace Microsoft.Graph
 {
-    public abstract class EncryptableSubscription : IEncryptableSubscription
+    public static class SubscriptionExtension
     {
-        public abstract string EncryptionCertificate { get; set; }
-
         /// <summary>
         /// Adds the encryption certificate information for change notifications with resource data to the subscription creation information.
         /// </summary>
         /// <param name="certificate">Certificate to use for encryption</param>
-        public void AddEncryptionCertificate(X509Certificate2 certificate)
+        public static void AddEncryptionCertificate(this IEncryptableSubscription subscription, X509Certificate2 certificate)
         {
-            EncryptionCertificate = Convert.ToBase64String(certificate.Export(X509ContentType.SerializedCert));
+            subscription.EncryptionCertificate = Convert.ToBase64String(certificate.Export(X509ContentType.SerializedCert));
         }
     }
 }
