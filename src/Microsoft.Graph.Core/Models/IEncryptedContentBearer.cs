a3fed46f7732746699564d5574c8a0639b295576
Vincent Biret <vincentbiret@hotmail.com>
2020-05-11 10:08:17 -0400

    - linting

--- src/Microsoft.Graph.Core/Models/IEncryptedContentBearer.cs
+++ src/Microsoft.Graph.Core/Models/IEncryptedContentBearer.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Microsoft.Graph
+﻿namespace Microsoft.Graph
 {
     public interface IEncryptedContentBearer
     {
