9345d48d435145121436253e66ca55bc96fa01e6
Vincent Biret <vincentbiret@hotmail.com>
2020-05-13 08:29:32 -0400

    - adds generic type constraint for deserialization

--- src/Microsoft.Graph.Core/Extensions/IDecryptableContentExtensions.cs
+++ src/Microsoft.Graph.Core/Extensions/IDecryptableContentExtensions.cs
@@ -24,7 +24,7 @@ namespace Microsoft.Graph
         /// <typeparam name="T">Type to deserialize the data to.</typeparam>
         /// <param name="certificateProvider">Certificate provider to decrypt the content. The first parameter is the certificate ID provided when creating the subscription. The second is the certificate thumbprint. The certificate WILL be disposed at the end of decryption.</param>
         /// <returns>Decrypted content as the provided type.</returns>
-        public static async Task<T> Decrypt<T>(this IDecryptableContent encryptedContent, Func<string, string, Task<X509Certificate2>> certificateProvider)
+        public static async Task<T> Decrypt<T>(this IDecryptableContent encryptedContent, Func<string, string, Task<X509Certificate2>> certificateProvider) where T : class
         {
             return JsonSerializer.Deserialize<T>(await encryptedContent.Decrypt(certificateProvider));
         }
