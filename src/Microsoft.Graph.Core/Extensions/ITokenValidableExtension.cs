1615df00400135c98f4db1c67e4f73dc8d3628af
Eastman <andrueastman@users.noreply.github.com>
2021-07-06 09:34:58 +0300

    Rich notifications support (#269)
    * - adds certificate serialization for subscription
    
    * - adds notification collection token validation
    
    * - moves certificate encryption method to extension to solve double inheritance
    
    * - moves token validation methods to extension
    
    * - renames extension method file to align with others
    
    * - moves the decryption methods to an extension method to align
    
    * - renames to plural to align
    
    * - linting
    
    * - Applies suggestions from code review
    
    * - replaces deserialization by system.text.json
    
    * - adds generic type constraint for deserialization
    
    * - removes unecessary lazy "constant" provider
    
    * Fix compiler warnings and cleanup calls
    
    * Fix compiler errors when used with service library
    
    * Ensure certificate is only passed with public key
    
    * Adds unit test to validate encryption and certificate handling
    
    * PR review feedback
    
    - convert using blocks to using statements
    - add exception docs in comments
    - Makes doc comments length shorter
    -
    
    * PR review feedback: Add defensive checks to public methods
    
    Co-authored-by: Vincent Biret <vincentbiret@hotmail.com>

---
+++ src/Microsoft.Graph.Core/Extensions/ITokenValidableExtension.cs
@@ -0,0 +1,69 @@
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+namespace Microsoft.Graph
+{
+    using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+    using Microsoft.IdentityModel.Tokens;
+    using Microsoft.IdentityModel.Protocols;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.IdentityModel.Tokens.Jwt;
+
+    /// <summary>
+    /// Contains extension methods for <see cref="ITokenValidableExtension"/>
+    /// </summary>
+    public static class ITokenValidableExtension
+    {
+        /// <summary>
+        /// Validates tokens attached with the notification collection. If the result is false, the notification collection should be discarded.
+        /// </summary>
+        /// <param name="collection">Collection instance of <see cref="ITokenValidable{T1,T2}"/></param>
+        /// <param name="tenantIds">List of tenant ids that notifications might be originating from.</param>
+        /// <param name="appIds">List of application id (client ids) that subscriptions have been created from.</param>
+        /// <param name="wellKnownUri">Well known URL to get the signing certificates for the tokens.
+        /// If you are not using the public cloud you need to pass the value corresponding to your national deployment.</param>
+        /// <param name="issuerPrefix">Issuer prefix for the "aud" claim in the tokens.
+        /// If you are not using the public cloud you need to pass the value corresponding to your national deployment.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="tenantIds"/> or <paramref name="appIds"/> is null or empty</exception>
+        /// <returns>Are tokens valid or not.</returns>
+        public static async Task<bool> AreTokensValid<T1,T2>(this ITokenValidable<T1, T2> collection, IEnumerable<Guid> tenantIds, IEnumerable<Guid> appIds,
+            string wellKnownUri = "https://login.microsoftonline.com/common/.well-known/openid-configuration",
+            string issuerPrefix = "https://sts.windows.net/") where T1 : IEncryptedContentBearer<T2> where T2 : IDecryptableContent
+        {
+            if ((collection.ValidationTokens == null || !collection.ValidationTokens.Any()) && collection.Value.All(x => x.EncryptedContent == null))
+                return true;
+            
+            if (tenantIds == null || !tenantIds.Any())
+                throw new ArgumentNullException(nameof(tenantIds));
+            if (appIds == null || !appIds.Any())
+                throw new ArgumentNullException(nameof(appIds));
+
+            var configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(wellKnownUri, new OpenIdConnectConfigurationRetriever());
+            var openIdConfig = await configurationManager.GetConfigurationAsync();
+            var handler = new JwtSecurityTokenHandler();
+            var issuersToValidate = tenantIds.Select(x => $"{issuerPrefix}{x}/");
+            var appIdsToValidate = appIds.Select(x => x.ToString());
+            return collection.ValidationTokens.Select(x => IsTokenValid(x, handler, openIdConfig, issuersToValidate, appIdsToValidate))
+                        .Aggregate((z, y) => z && y);
+        }
+
+        private static bool IsTokenValid(string token, JwtSecurityTokenHandler handler, OpenIdConnectConfiguration openIdConfig, IEnumerable<string> issuersToValidate, IEnumerable<string> appIds)
+        {
+            handler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidateLifetime = true,
+                ValidIssuers = issuersToValidate,
+                ValidAudiences = appIds,
+                IssuerSigningKeys = openIdConfig.SigningKeys
+            }, out _);
+            return true;
+        }
+    }
+}
