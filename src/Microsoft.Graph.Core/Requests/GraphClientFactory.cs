a5011989e45177a659a85b607623a9a3a3fa35be
Andrew Omondi <anomondi@microsoft.com>
2024-07-24 10:37:13 +0300

    Cleanup out of date code

--- src/Microsoft.Graph.Core/Requests/GraphClientFactory.cs
+++ src/Microsoft.Graph.Core/Requests/GraphClientFactory.cs
@@ -170,20 +170,6 @@ namespace Microsoft.Graph
                     throw new ArgumentNullException(nameof(handlers), "DelegatingHandler array contains null item.");
                 }
 
-#if IOS || MACOS || MACCATALYST
-#if IOS || MACCATALYST
-                // Skip CompressionHandler since NSUrlSessionHandler automatically handles decompression on iOS and macOS and it can't be turned off.
-                // See issue https://github.com/microsoftgraph/msgraph-sdk-dotnet/issues/481 for more details.
-                if (finalHandler.GetType().Equals(typeof(NSUrlSessionHandler)) && handler.GetType().Equals(typeof(CompressionHandler)))
-#elif MACOS
-                if (finalHandler.GetType().Equals(typeof(Foundation.NSUrlSessionHandler)) && handler.GetType().Equals(typeof(CompressionHandler)))
-#endif
-                {
-                    // Skip chaining of CompressionHandler.
-                    continue;
-                }
-#endif
-
                 // Check for duplicate handler by type.
                 if (!existingHandlerTypes.Add(handler.GetType()))
                 {
