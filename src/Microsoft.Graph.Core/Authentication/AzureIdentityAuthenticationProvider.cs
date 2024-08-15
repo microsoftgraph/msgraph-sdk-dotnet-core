2dad161139f44059cab3c248ef47fef82216cf4f
Vincent Biret <vibiret@microsoft.com>
2024-08-15 12:57:58 -0400

    fix: missing constructor for CAE
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Microsoft.Graph.Core/Authentication/AzureIdentityAuthenticationProvider.cs
+++ src/Microsoft.Graph.Core/Authentication/AzureIdentityAuthenticationProvider.cs
@@ -2,6 +2,7 @@
 //  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 // ------------------------------------------------------------------------------
 
+using System;
 using Azure.Core;
 using Microsoft.Kiota.Abstractions.Authentication;
 
@@ -18,7 +19,19 @@ public class AzureIdentityAuthenticationProvider : BaseBearerTokenAuthentication
     /// <param name="allowedHosts">The list of allowed hosts for which to request access tokens.</param>
     /// <param name="scopes">The scopes to request the access token for.</param>
     /// <param name="observabilityOptions">The observability options to use for the authentication provider.</param>
-    public AzureIdentityAuthenticationProvider(TokenCredential credential, string[] allowedHosts = null, Microsoft.Kiota.Authentication.Azure.ObservabilityOptions observabilityOptions = null, params string[] scopes) : base(new AzureIdentityAccessTokenProvider(credential, allowedHosts, observabilityOptions, scopes))
+    /// <param name="isCaeEnabled">Determines if the Continuous Access Evaluation (CAE) is enabled.</param>
+    public AzureIdentityAuthenticationProvider(TokenCredential credential, string[] allowedHosts = null, Microsoft.Kiota.Authentication.Azure.ObservabilityOptions observabilityOptions = null, bool isCaeEnabled = true, params string[] scopes) : base(new AzureIdentityAccessTokenProvider(credential, allowedHosts, observabilityOptions, isCaeEnabled, scopes))
+    {
+	}
+    /// <summary>
+    /// The <see cref="AzureIdentityAuthenticationProvider"/> constructor
+    /// </summary>
+    /// <param name="credential">The credential implementation to use to obtain the access token.</param>
+    /// <param name="allowedHosts">The list of allowed hosts for which to request access tokens.</param>
+    /// <param name="scopes">The scopes to request the access token for.</param>
+    /// <param name="observabilityOptions">The observability options to use for the authentication provider.</param>
+    [Obsolete("Use the constructor that takes an isCaeEnabled parameter instead.")]
+    public AzureIdentityAuthenticationProvider(TokenCredential credential, string[] allowedHosts = null, Microsoft.Kiota.Authentication.Azure.ObservabilityOptions observabilityOptions = null, params string[] scopes) : this(credential, allowedHosts, observabilityOptions, true, scopes)
     {
 	}
 }
