41517b26abdb6109f61c5a98442177409661ca61
Andrew Omondi <andrueastman@users.noreply.github.com>
2024-09-10 16:38:04 +0300

    Update src/Microsoft.Graph.Core/Requests/BatchRequestBuilder.cs
    Co-authored-by: Vincent Biret <vibiret@microsoft.com>

--- src/Microsoft.Graph.Core/Requests/BatchRequestBuilder.cs
+++ src/Microsoft.Graph.Core/Requests/BatchRequestBuilder.cs
@@ -108,8 +108,7 @@ namespace Microsoft.Graph.Core.Requests
         {
             if (httpResponseMessage.IsSuccessStatusCode) return;
 
-            var contentTypeString = httpResponseMessage.Content?.Headers?.ContentType?.MediaType is { } contentTypeMediaType && !string.IsNullOrEmpty(contentTypeMediaType) ? contentTypeMediaType : string.Empty;
-            if (httpResponseMessage.Content != null && contentTypeString.StartsWith(CoreConstants.MimeTypeNames.Application.Json, StringComparison.OrdinalIgnoreCase))
+            if (httpResponseMessage is {Content.Headers.ContentType.MediaType: string contentTypeMediaType} && !string.IsNullOrEmpty(contentTypeMediaType) && contentTypeString.StartsWith(CoreConstants.MimeTypeNames.Application.Json, StringComparison.OrdinalIgnoreCase))
             {
                 using var responseContent = await httpResponseMessage.Content.ReadAsStreamAsync().ConfigureAwait(false);
                 using var document = await JsonDocument.ParseAsync(responseContent).ConfigureAwait(false);
